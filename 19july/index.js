859e7daf22d83056ead135e5b0c35adc235bb18a
chandan <chandankumar.btele20@pec.edu.in>
2024-07-19 12:17:07 +0530

    19july

---
+++ 19july/index.js
@@ -0,0 +1,141 @@
+
+
+// {
+//     let a=5;
+//     {
+//         console.log(a)
+//     }
+    
+// }
+
+
+
+// {
+//     {
+//         let a=8
+//         {
+//             {
+//                 console.log(a)
+//             }
+         
+//         }
+
+//         console.log(a)
+//     }
+  
+//     {
+
+        
+//         // console.log(a)
+//     }
+// }
+// // let a=5;
+
+// let i;
+// i++   i will beacome i+1, i+=2,   i will beacome i+2;
+//                           i+=num  i will beacome i+num
+                        //   i-=num  i will beacome i-num
+                        //   i*=num  i will become  i*num
+                        //   i/=num  i will become  i/num
+// 
+    //start    end condition     incremnt/decrement
+    
+// for(let i=0;      i<=5;                i++){
+//         console.log(i)
+// }
+
+
+// for(let i=0;      i<=5;                i+=2){
+//     console.log(i)
+// }
+
+// for(let i=0;      i<=48;                i*=2){
+//     console.log(i)
+// }
+
+
+//print 1000000,100000,10000,1000,100,10,1
+// for(let i=1000000;  i>=1;  i/=10){
+
+//     console.log(i)
+// }
+
+// const a=8;
+
+// a=7;
+
+// {
+//     {
+//         {
+//             console.log(a)
+//         }
+//     }
+// }
+
+
+// {
+//     const a=8;
+//     console.log(a)
+// }
+// let name="aman"
+let print=function(){
+    // let name="aman arash"
+    // console.log(name)
+}
+
+print()
+// console.log(name)
+
+// var name="aman";
+// console.log(name)
+
+// let name="aras"
+// name=name+"h";
+// console.log(name+"deep");
+
+// for(let i=1;i<=9;i++){
+//     let currans="";
+//     for(let j=1;j<=i;j++){
+//         currans=currans+""+j;
+//     }
+//     console.log(currans);
+// }
+
+for(let star=5; star>=1; star-- ){
+    let currstar="";
+    for(let i=1;i<=star;i++){
+        currstar=currstar+"*"
+
+    }
+   
+
+    console.log(currstar)
+}
+
+for(let star=2; star<=5; star++ ){
+    let currstar="";
+    for(let i=1;i<=star;i++){
+        currstar=currstar+"*"
+
+    }
+   
+
+    console.log(currstar)
+}
+
+
+
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+// 123456
+// 1234567
+// 12345678
+// 123456789
+
+
+
+
